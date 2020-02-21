fe08336b0809b91a0b695dd022d44ebe79b8549f
JasonWu008 <1179858091@qq.com>
2020-02-21 13:51:45 +0800

    完成魔方的基本功能

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <rubic></rubic>
+  </div>
+</template>
+
+<script>
+import Rubic from './components/Rubic'
+
+export default {
+  name: 'App',
+  components: {
+    Rubic
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Rubic.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <div id="rubic">
+    </div>
+  </div>
+</template>
+
+<script>
+import { threeStart } from '../utils/Rubic.js'
+export default {
+  name: 'Rubic',
+  mounted () {
+    threeStart()
+  }
+}
+</script>
+
+<style scoped>
+#rubic {
+  cursor: pointer;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  margin: -15px;
+}
+</style>
